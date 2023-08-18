84ee73eb64e014646664f5ef80af3a307346cdb5
Lucas-ggarcia <lucasgeacia@gmail.com>
2023-08-17 21:53:46 -0300

    adicionado exercícios de calculo

---
+++ index.js
@@ -0,0 +1,21 @@
+const Soma1 = 3 +5
+const Multi1 = (3 * 5)/2
+const Subt1 = (4 - 5) * -1 
+const restoDivisao = 234 % 5
+
+document.write(`A soma é igual a ${Soma1}
+A divisão do produto é ${Multi1}
+A multiplicação da subtração é ${Subt1}
+O resto da divisão é ${restoDivisao}`)
+
+alert(`A soma é igual a ${Soma1}
+A divisão do produto é ${Multi1}
+A multiplicação da subtração é ${Subt1}
+O resto da divisão é ${restoDivisao}`)
+
+console.log(`A soma é igual a ${Soma1}
+A divisão do produto é ${Multi1}
+A multiplicação da subtração é ${Subt1}
+O resto da divisão é ${restoDivisao}`)
+
+
